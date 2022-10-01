bedae8e2f11362c188a01c760a58a29b763b7e96
Jamshid <jamshid@novalab.uz>
2022-10-01 16:56:27 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:bloc_lesson/count/count_bloc_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'count/count_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => CountCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
+}
+
+
